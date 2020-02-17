ed18f20bfa8aba3db55a9e4c9a7130d9499dbaa3
juanM20 <jmtp12poke@gmail.com>
2020-02-17 14:46:47 -0600

    Vocabulario listo :3

--- Text_normalization/Vocabulary.py
+++ Text_normalization/Vocabulary.py
@@ -6,18 +6,18 @@ from nltk.corpus import wordnet
 from nltk import word_tokenize
 
 
-def remove_char_2(tokens):
-   
-    for token in tokens:
-        rep = re.sub(PATTERN, r' ', token)
-        token = rep
-    return tokens
-
-
 def remove_characters_after_tokenization(tokens):
     pattern = re.compile('[{}]'.format(re.escape(string.punctuation))) 
     filtered_tokens = filter(None, [pattern.sub('', token) for token in tokens]) 
     return filtered_tokens
+
+
+def remove_characters_before_tokenization(token):
+    
+    PATTERN = r'\?|\$|&|\*|%|@|(|)|~|(-)|[0-9]|¿|¡|—|¦|º|”|;|,|\[|\]|\{|\}|#|:|\/|\\' # add other characters here to remove them
+    filtered_sentence = re.sub(PATTERN, r'', token)
+
+    return filtered_sentence
 
 
 
@@ -36,20 +36,29 @@ if __name__ == '__main__':
     soup = BeautifulSoup(text, 'lxml')
     text = soup.get_text()
 
-    token_list = word_tokenize(text)
+    token_list = word_tokenize(text.lower())
    
     filter1 = remove_characters_after_tokenization(token_list)
     filter_1 = list(filter1)
     
-    filter_2 = sorted(list(set(filter_1)))
+    filter_2 = list(set(filter_1))
+        
+    filter_3 = [remove_characters_before_tokenization(token) for token in filter_2 
+                if len(remove_characters_before_tokenization(token)) > 0]
     
-    filter_3 = remove_stopwords(filter_2)
-   
-    
-    PATTERN = r'[^a-z]'
+    filter_4 = remove_stopwords(filter_3)
 
-    filter_4 = [token for token in filter_3 if len(re.sub(PATTERN, r'', token)) > 0 ]
-    print(filter_4)
+    filter_5 = sorted(set(filter_4))
+    
+    #print(filter_5)
+    print(len(filter_5))
+
+
+    vocabulary = open('vocabulary.txt', 'w')
+    for word in filter_5:
+        vocabulary.write(word+'\n')
+    vocabulary.close()
+
 
 
 
